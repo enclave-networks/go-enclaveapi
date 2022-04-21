c2123e6552df9cff21a161c2d2e25e8472f195fa
Thomas Soulard <tom.soulard@enclave.io>
2022-04-21 12:47:14 +0100

    Fixed martialing of enclavekeycreate

--- data/enrolment_key_create.go
+++ data/enrolment_key_create.go
@@ -1,12 +1,12 @@
 package data
 
 type EnrolmentKeyCreate struct {
-	Type                         EnrolmentKeyType
-	ApprovalMode                 EnrolmentKeyApprovalMode
-	Description                  string
-	UsesRemaining                int
-	IpConstraints                []EnrolmentKeyIpConstraint
-	Tags                         []string
-	DisconnectedRetentionMinutes int
-	Notes                        string
+	Type                         EnrolmentKeyType           `json:"Type,omitempty"`
+	ApprovalMode                 EnrolmentKeyApprovalMode   `json:"ApprovalMode,omitempty"`
+	Description                  string                     `json:"Description,omitempty"`
+	UsesRemaining                int                        `json:"UsesRemaining,omitempty"`
+	IpConstraints                []EnrolmentKeyIpConstraint `json:"IpConstraints,omitempty"`
+	Tags                         []string                   `json:"Tags,omitempty"`
+	DisconnectedRetentionMinutes int                        `json:"DisconnectedRetentionMinutes,omitempty"`
+	Notes                        string                     `json:"Notes,omitempty"`
 }
