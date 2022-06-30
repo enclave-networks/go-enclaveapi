ca3e33e39ba86c4d7a9adfcbd503b48cfd38a70e
Thomas Soulard <tom.soulard@enclave.io>
2022-06-30 13:37:08 +0100

    added trust requirement ID to policy

--- data/policy/policy.go
+++ data/policy/policy.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/enclave-networks/go-enclaveapi/data"
+	"github.com/enclave-networks/go-enclaveapi/data/trustrequirement"
 )
 
 // An int backed Policy Id.
@@ -11,13 +12,14 @@ type PolicyId int
 
 // Represents a single policy.
 type Policy struct {
-	Id           PolicyId
-	Created      time.Time
-	Description  string
-	IsEnabled    bool
-	State        PolicyState
-	SenderTags   []data.TagReference
-	ReceiverTags []data.TagReference
-	Acls         []PolicyAcl
-	Notes        string
+	Id                PolicyId
+	Created           time.Time
+	Description       string
+	IsEnabled         bool
+	State             PolicyState
+	SenderTags        []data.TagReference
+	ReceiverTags      []data.TagReference
+	Acls              []PolicyAcl
+	Notes             string
+	TrustRequirements []trustrequirement.TrustRequirementId
 }
--- data/policy/policy_patch.go
+++ data/policy/policy_patch.go
@@ -1,11 +1,14 @@
 package policy
 
+import "github.com/enclave-networks/go-enclaveapi/data/trustrequirement"
+
 // Defines the modifiable properties of a policy.
 type PolicyPatch struct {
-	Description  string      `json:"Description,omitempty"`
-	IsEnabled    bool        `json:"IsEnabled,omitempty"`
-	SenderTags   []string    `json:"SenderTags,omitempty"`
-	ReceiverTags []string    `json:"ReceiverTags,omitempty"`
-	Acls         []PolicyAcl `json:"Acls,omitempty"`
-	Notes        string      `json:"Notes,omitempty"`
+	Description       string                                `json:"Description,omitempty"`
+	IsEnabled         bool                                  `json:"IsEnabled,omitempty"`
+	SenderTags        []string                              `json:"SenderTags,omitempty"`
+	ReceiverTags      []string                              `json:"ReceiverTags,omitempty"`
+	Acls              []PolicyAcl                           `json:"Acls,omitempty"`
+	Notes             string                                `json:"Notes,omitempty"`
+	TrustRequirements []trustrequirement.TrustRequirementId `json:"TrustRequirements,omitempty"`
 }
