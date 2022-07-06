828ef7f9cf8e43e61af365a184cc68d78fb58d9d
Thomas Soulard <tom.soulard@enclave.io>
2022-07-06 15:49:49 +0100

    correct conditions type

--- data/trustrequirement/trust_requirement_settings.go
+++ data/trustrequirement/trust_requirement_settings.go
@@ -1,6 +1,6 @@
 package trustrequirement
 
 type TrustRequirementSettings struct {
-	Configuration map[string]string `json:"Configuration,omitempty"`
-	Conditions    map[string]string `json:"Conditions,omitempty"`
+	Configuration map[string]string   `json:"Configuration,omitempty"`
+	Conditions    []map[string]string `json:"Conditions,omitempty"`
 }
